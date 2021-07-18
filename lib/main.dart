133a494a65dc876496f48f6aa584b44c2cf5ed21
ttury <lovekj966@gmail.com>
2021-07-18 21:42:33 +0900

    volume handling module add

--- lib/main.dart
+++ lib/main.dart
@@ -30,12 +30,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  AudioPlayer player = AudioPlayer();
-  AudioCache cache = AudioCache();
+  AudioPlayer player = AudioPlayer(); // 오디오 조정에 사용하는 클래스
+  AudioCache cache = AudioCache(); // 오디오 로컬 재생(휴대폰)에 사용하는 클래스
   Color color = Colors.white;
-  String audioPath = "sample.mp3";
+  String audioPath = "sample.mp3"; // assets 폴더의 sample.mp3 확인
   bool isPlaying = false;
   bool isPlayedOnce = false;
+  double volume = 1; // 기본 볼륨 설정(0부터 1 사이 값만 유효)
+  double volume_interval = 0.2; // 한번 조정할 때마다 0.2씩 볼륨이 바뀜(조정 가능)
 
   void _playFile() async{
     if (!isPlayedOnce) {
@@ -49,14 +51,18 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  void _stopFile() {
-    player.stop(); // stop the file like this
+  void _stopFile() { // 오디오 멈추는 명령어
+    player.stop();
     isPlaying = false;
   }
 
-  void _pauseFile() {
-    player.pause(); // stop the file like this
+  void _pauseFile() { // 오디오 일시정지 명령어
+    player.pause();
     isPlaying = false;
+  }
+
+  void _changeVolume(double volume) {
+    player.setVolume(volume);
   }
 
   @override
@@ -85,6 +91,8 @@ class _MyHomePageState extends State<MyHomePage> {
           onHorizontalDragEnd: (DragEndDetails details){
             setState(() {
               color = Colors.greenAccent;
+              volume < 1 ? volume += volume_interval : volume = 1;
+              _changeVolume(volume);
             });
           },
           onVerticalDragStart: (DragStartDetails details){
@@ -96,20 +104,24 @@ class _MyHomePageState extends State<MyHomePage> {
             print(details.primaryVelocity);
             setState(() {
               color = Colors.black;
-              _stopFile(); //중지(오디오 처음으로)
+              volume > 0 ? volume -= volume_interval : volume = 0;
+              _changeVolume(volume);
             });
           },
           onLongPress: (){
             setState(() {
               color = Colors.black12;
+              _stopFile(); //중지(오디오 처음으로)
             });
           },
+
           child: Container(
             color: color,
             height: 1920,
             width: 1080,
+            child: Text("volume = " + volume.toString()),
           ),
-        ),
+          ),
       ),
     );
   }
